5fedce3e9ca45c024f003d872ee8150226e49050
Staceka1 <staceyarogers@outlook.com>
2023-11-30 14:40:26 +1100

    Commit basic elements on page

---
+++ assets/script.js
@@ -0,0 +1,3 @@
+// External files make your code organized and easier to maintain
+// You can find this console when you go to inspect, click console.
+console.log("This is the log for Javascript");
